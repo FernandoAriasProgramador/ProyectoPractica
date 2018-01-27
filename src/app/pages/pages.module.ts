e34157ead4dc4169e3b1743651a51d17442a7256
Fernando <jf.fernando.arias@gmail.com>
2018-01-27 04:57:57 -0300

    seccion 8 terminada

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -12,6 +12,8 @@ import { IncrementadorComponent } from '../components/incrementador/incrementado
 import { ChartsModule } from 'ng2-charts';
 import { DonasComponent } from '../components/donas/donas.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 
 
@@ -23,7 +25,9 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
         PagesComponent,
         IncrementadorComponent,
         DonasComponent,
-        AccountSettingsComponent
+        AccountSettingsComponent,
+        PromesasComponent,
+        RxjsComponent
     ],
     exports: [
         DashboardComponent,
